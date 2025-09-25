a93a2f029c5b0e92c4af4eb6241ec5934c4503dc
victor-hugo <hugovco511@gmail.com>
2025-09-24 23:15:59 -0300

    refactor: propriedade do env que não usa mais

--- config.php
+++ config.php
@@ -3,7 +3,6 @@ require_once __DIR__ . '/app/Utils/ManageEnv.php';
 
 ManageEnv::loadEnv(__DIR__ . '/.env');
 
-define('BASE_URL', $_ENV['BASE_URL'] ?? 'http://127.0.0.1/');
 define('DB_HOST', $_ENV['DB_HOST'] ?? '127.0.0.1');
 define('DB_USER', $_ENV['DB_USER'] ?? 'root');
 define('DB_PASS', $_ENV['DB_PASS'] ?? '');
